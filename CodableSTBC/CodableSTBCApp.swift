fd840954254bebc27a666460bad3c412334ed631
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-31 16:28:16 +0300

    Initial Commit

---
+++ CodableSTBC/CodableSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodableSTBCApp.swift
+//  CodableSTBC
+//
+//  Created by Vahtee Boo on 31.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CodableSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
